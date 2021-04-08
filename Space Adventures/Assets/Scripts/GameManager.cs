8da337eeed73124c649bdf5e9f4403bbda289af9
thea-t <58753010+thea-t@users.noreply.github.com>
2021-04-09 00:44:32 +0300

    initial

---
+++ Space Adventures/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+
+    public static GameManager Instance { get; private set; } // static singleton
+    
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; }
+        else { Destroy(gameObject); }
+    }
+
+
+    }
---
+++ Space Adventures/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] private int fuel = 20;
+    [SerializeField] private int shipSpeed = 20;
+
+    void Update()
+    {
+       
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * shipSpeed);
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * shipSpeed);
+        } 
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * shipSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * shipSpeed);
+        }
+    }
+
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("planet"))
+        {
+            other.gameObject.GetComponent<GravityController>().playerIsInRange = true;
+        }
+        
+    } 
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("planet"))
+        {
+            other.gameObject.GetComponent<GravityController>().playerIsInRange = false;
+    }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("planet"))
+        {
+            
+        }
+    }
+    }
+

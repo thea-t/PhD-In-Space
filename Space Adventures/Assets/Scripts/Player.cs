b2d89ce5c1d91ee33d0edba78367dff0b657ca44
thea-t <58753010+thea-t@users.noreply.github.com>
2021-04-09 21:01:25 +0300

    cameras setup

--- Space Adventures/Assets/Scripts/Player.cs
+++ Space Adventures/Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
-    [SerializeField] private int fuel = 20;
+    [SerializeField] private float fuel = 20;
+    [SerializeField] private float fuelConsumption = 1;
     [SerializeField] private int shipSpeed = 20;
 
     void Update()
@@ -15,20 +16,25 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(Vector3.forward * shipSpeed);
+            OnFuelOver();
         }
         
         if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(Vector3.back * shipSpeed);
+            OnFuelOver();
         } 
 
         if (Input.GetKey(KeyCode.A))
         {
             rb.AddForce(Vector3.left * shipSpeed);
+            OnFuelOver();
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(Vector3.right * shipSpeed);
+            OnFuelOver();
         }
     }
 
@@ -56,6 +62,19 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("planet"))
         {
             
+        }
+    }
+
+    void OnFuelOver()
+    {
+        if (fuel<1)
+        {
+            shipSpeed = 0;
+        }
+
+        else
+        {
+            fuel -= fuelConsumption * 0.01f;
         }
     }
     }

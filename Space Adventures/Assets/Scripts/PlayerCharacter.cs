af3d3ff97f8412813d6c309780490673afc54704
thea-t <teodoratx3@gmail.com>
2021-04-19 03:07:16 +0300

    imported assets

--- Space Adventures/Assets/Scripts/PlayerCharacter.cs
+++ Space Adventures/Assets/Scripts/PlayerCharacter.cs
@@ -6,8 +6,12 @@ namespace SpaceAdventures
 
     public class PlayerCharacter : MonoBehaviour
     {
-        [Range(1, 10)][SerializeField] private int playerSpeed;
+        /*[Range(1000, 10000)] */[SerializeField] private int playerSpeed;
         [SerializeField] private Rigidbody rb;
+        [SerializeField] Transform playerModel;
+        [SerializeField] Animator playerAnimator;
+
+
         private void Start()
         {
             GameManager.Instance.curvedWorldController.bendPivotPoint = transform;
@@ -20,24 +24,67 @@ namespace SpaceAdventures
 
         void CheckKeyForPlayerMovement()
         {
+
             if (Input.GetKey(KeyCode.A))
             {
-                rb.AddForce(Vector3.left * playerSpeed*100);
+                rb.AddForce(Vector3.left * playerSpeed * Time.deltaTime);
+                playerAnimator.SetBool("Run", true);
+                RotateCharacter();
             }
+            else if (Input.GetKeyUp(KeyCode.A))
+            {
+                playerAnimator.SetBool("Run", false);
+            }
+
+
             if (Input.GetKey(KeyCode.D))
             {
-                rb.AddForce(Vector3.right * playerSpeed*100);
+                rb.AddForce(Vector3.right * playerSpeed * Time.deltaTime);
+                playerAnimator.SetBool("Run", true);
+                RotateCharacter();
             }
+            else if (Input.GetKeyUp(KeyCode.D))
+            {
+                playerAnimator.SetBool("Run", false);
+            }
+
+
             if (Input.GetKey(KeyCode.W))
             {
-                rb.AddForce(Vector3.forward * playerSpeed*100);
+                rb.AddForce(Vector3.forward * playerSpeed * Time.deltaTime);
+                playerAnimator.SetBool("Run", true);
+                RotateCharacter();
             }
+            else if (Input.GetKeyUp(KeyCode.W))
+            {
+                playerAnimator.SetBool("Run", false);
+            }
+
             if (Input.GetKey(KeyCode.S))
             {
-                rb.AddForce(Vector3.back * playerSpeed*100);
+                rb.AddForce(Vector3.back * playerSpeed * Time.deltaTime);
+                playerAnimator.SetBool("Run", true);
+                RotateCharacter();
+            }
+            else if (Input.GetKeyUp(KeyCode.S))
+            {
+                playerAnimator.SetBool("Run", false);
+            }
+
+
+
+        }
+
+        void RotateCharacter()
+        {
+            if (rb.velocity != Vector3.zero)
+            {
+                //https://docs.unity3d.com/ScriptReference/Quaternion.LookRotation.html
+                playerModel.rotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
             }
         }
 
     }
 }
+
 

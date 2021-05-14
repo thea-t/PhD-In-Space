552f75005e86f8b20ff96b39ae765718256a09b2
thea-t <teodoratx3@gmail.com>
2021-05-14 07:21:39 +0300

    Custom animations to the melee enemies of planet Aeriaeria, Created 10 different animators with custom animations for the ranged enemies. Overall, the first planet is 90% ready

---
+++ Space Adventures/Assets/_My Assets/Scripts/EnemyRanged.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRanged : Enemy
+{
+    [SerializeField] Bullet bulletPrefab;
+    [SerializeField] GameObject bulletShootPoint;
+
+
+
+    protected override void StartChasing()
+    {
+        base.StartChasing();
+            m_animator.SetBool("Run", true);
+            m_animator.SetBool("Projectile Right Attack 01", true);
+    }
+
+    protected override void StopChasing()
+    {
+            base.StopChasing();
+        m_animator.SetBool("Run", false);
+        m_animator.SetBool("Projectile Right Attack 01", false);
+    }
+
+    //animator event
+    void OnShoot()
+    {
+        if (!GameManager.Instance.playerCharacter.isDead && !isDead)
+        {
+            Bullet bullet = Instantiate(bulletPrefab, bulletShootPoint.transform.position, transform.rotation);
+            Vector3 shootPointPos = new Vector3(bulletShootPoint.transform.position.x, 0, bulletShootPoint.transform.position.z);
+            bullet.directionVector = (GameManager.Instance.playerCharacter.transform.position - shootPointPos).normalized;
+            Destroy(bullet.gameObject, 2);
+        }
+    }
+
+}
